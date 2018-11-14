0e3d2864b3eecd933c0196dfcc2bd43f33b2545a
Linus Lotz <l.lotz@reply.de>
2018-11-14 15:32:13 +0100

    add git to php containers

--- update.sh
+++ update.sh
@@ -72,7 +72,7 @@ do
     pecl_extensions=""
     php_extensions=""
     build_deps=""
-    run_deps="bash "
+    run_deps="bash git "
     for ext in $(cat ${DIR}/${framework}/deps)
     do
         add_extension $ext
